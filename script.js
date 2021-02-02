9e78a16cc2fe7d137de6f0a5dc38252eb5bf45a3
Hong Tran <jessietran@fb.com>
2021-02-02 11:53:16 -0800

    tried to embed Text-to-speech of gg api but have a problem with authentication

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const messages= [
     {
         name: "Mike",
@@ -52,9 +53,63 @@ const messages= [
 ]
 
 let i = 0
-let message = ""
 let messageText = document.getElementById('message')
 let name = document.getElementById('name')
+
+function speak(text){
+    if ('speechSynthesis' in window) {
+        console.log("trying to speak")
+        // Speech Synthesis supported 🎉
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+    }else{
+        // Speech Synthesis Not Supported 😣
+        alert("Sorry, your browser doesn't support text to speech!");
+    }
+}
+
+function speakGoogleAPI(text){
+    const request = {
+        "input":{
+            "text": text
+          },
+          "voice":{
+            "languageCode":"en-gb",
+            "name":"en-GB-Standard-A",
+            "ssmlGender":"FEMALE"
+          },
+          "audioConfig":{
+            "audioEncoding":"MP3"
+          }
+    }
+    try {
+        var key = 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDSblX986VeLnAx\nQy0iBqalkO9L/cCZstbgLji0vJLHlB2j3oCGD+wR9eWCnuKHDZqwEmJvMH1WUrO2\nk8Rpj+Po4Uf3+HqQswvqroFb4FpBFb6SA0aWf8D1ar8tSs3gNas8S8/wyIgvC5LW\nry/lVu16QrLy5/Tcbny+SzZUvAKWavsoOvGVs2zIf2U3cc4Hda4HfGzt032CEFKl\n+Rb+NP9H3Voc1vT8p2cgTU04AXn1brveXAw7tcTkauU1+E0Qnb2ygWLu5yy2oUyd\nEBW7RkPP9jkGzAXi5JFzBM7PmDjxJPBCdHfZg7ltYJYMFjgAvtGV+ht+TYyCN6vV\npNgNv1CBAgMBAAECggEACFNaGsHGPP5hfRp5PXmVw/LSu+8EjPQ14xFjGBgGpuev\n30cXLs6wFnMUFz2b7O/baxxUGY35pIXhwz9QD+Tj9ZedtER4DXPvDJRGTRU4nu76\nPu7LHvrbnp4gRZ2KPRTkgy6E8fPuKu3WW8D7SDOu8kWTrM+DeSn0LKd/RR2Z7Nto\n+dSPnqqOy1RxOY2XianlMevfXLkQgtzDflJgEBq0lGHu2Q2jDXkJlpcoGsCZNFHe\nFOl4fBUCmh5YxJeb5lgaCSf0Pu8D6B3g0zvE88v1LZ4dEBmSsT7RL32FwsixiXGH\ncBB+9H4uNThNSdQKMndltR+gBOYmpuWZ8wvxV/MdEQKBgQDquzmK8ubKqpSQ1roU\ndT6xp2OrjzZSDZWao+BoL3U43G80E8cIX6m7uLz9kdnvLwHlXPcPC+ZgcQz2oQMg\nAgOOUFMhOQ8fLhDaOn6ZMjEqgU5oTnQggTVIzrxtt/6OgsNNW2WZ4Kr/voqWmj60\ns0Ry26lxPB5hCSNdUiormMR3MQKBgQDlf3IeQDM/ikfaL08nrDluVwTtBfyQD8dd\npL+Cq9iq68DDOhCDKk0fWAOoVfjY4nHoVFTgGwvp/pybDm6e2pV7i+twTabzWdMZ\n7kFfpogBFIY3kCQIuFZtexLJlGYtaEWrp9Wbq4P27HkhHgAQSb9wYT2KZbXNIDW8\nV4rzQby6UQKBgQDjz8OAJFBzf2xiydOdCVFIcBGeroZkykRY6hVgaDbXcbEHlO5F\nJTsBMsFRQ92EclXfugMZek4jaXTQY+I5jVrQIwsbIlqXw0id08mMexcWjpp/Ry0T\nYvNI70QXJEyNGUHc8QhBeLfdL5snoRmcnKOg6amgeV1uhcTpzFWbLE/48QKBgQCU\nI/BGFqr+GQTIkkFGGqX307QaTAL7+zN1/yKf9s2lhwim/JQsFuIxmV7DCrvXRFTC\n46zl/I90PCZa8LX3VKGpXmhb1cQu0Yyy8FjCpXylNdptg12/Qz6G7SrzdBeAuA5A\n/9Wi5tQIUBzgg70GxpQMtrhC3eS8xE3rhZbr70MkAQKBgBZhkOE1tnFoJ+0MiPQ4\nTpg/BA3JJeZEYkLj4wkAmN5wAzm651Ql6hSBz/4BR7ZKdkT8WgdrDDtTubsFQ1RW\nYtsBTqTlqhYLVENMXgt7BBdGXRxU8aScE8LJcgic9vLD14BwvutFz7eOzozJD60Q\n6eFftoeqEbyw8m3uLwbMqtxR';
+        const url = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${key}`
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        // xhr.setRequestHeader('Access-Control-Allow-Credentials', true)
+        xhr.withCredentials = true;
+        xhr.send(JSON.stringify({
+            value: request
+        }));
+        // fetch(url, {
+        //     method: "POST", 
+        //     withCredentials: true,
+        //     credentials: 'include',
+        //     headers: {
+        //         'x-goog-api-key': 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDSblX986VeLnAx\nQy0iBqalkO9L/cCZstbgLji0vJLHlB2j3oCGD+wR9eWCnuKHDZqwEmJvMH1WUrO2\nk8Rpj+Po4Uf3+HqQswvqroFb4FpBFb6SA0aWf8D1ar8tSs3gNas8S8/wyIgvC5LW\nry/lVu16QrLy5/Tcbny+SzZUvAKWavsoOvGVs2zIf2U3cc4Hda4HfGzt032CEFKl\n+Rb+NP9H3Voc1vT8p2cgTU04AXn1brveXAw7tcTkauU1+E0Qnb2ygWLu5yy2oUyd\nEBW7RkPP9jkGzAXi5JFzBM7PmDjxJPBCdHfZg7ltYJYMFjgAvtGV+ht+TYyCN6vV\npNgNv1CBAgMBAAECggEACFNaGsHGPP5hfRp5PXmVw/LSu+8EjPQ14xFjGBgGpuev\n30cXLs6wFnMUFz2b7O/baxxUGY35pIXhwz9QD+Tj9ZedtER4DXPvDJRGTRU4nu76\nPu7LHvrbnp4gRZ2KPRTkgy6E8fPuKu3WW8D7SDOu8kWTrM+DeSn0LKd/RR2Z7Nto\n+dSPnqqOy1RxOY2XianlMevfXLkQgtzDflJgEBq0lGHu2Q2jDXkJlpcoGsCZNFHe\nFOl4fBUCmh5YxJeb5lgaCSf0Pu8D6B3g0zvE88v1LZ4dEBmSsT7RL32FwsixiXGH\ncBB+9H4uNThNSdQKMndltR+gBOYmpuWZ8wvxV/MdEQKBgQDquzmK8ubKqpSQ1roU\ndT6xp2OrjzZSDZWao+BoL3U43G80E8cIX6m7uLz9kdnvLwHlXPcPC+ZgcQz2oQMg\nAgOOUFMhOQ8fLhDaOn6ZMjEqgU5oTnQggTVIzrxtt/6OgsNNW2WZ4Kr/voqWmj60\ns0Ry26lxPB5hCSNdUiormMR3MQKBgQDlf3IeQDM/ikfaL08nrDluVwTtBfyQD8dd\npL+Cq9iq68DDOhCDKk0fWAOoVfjY4nHoVFTgGwvp/pybDm6e2pV7i+twTabzWdMZ\n7kFfpogBFIY3kCQIuFZtexLJlGYtaEWrp9Wbq4P27HkhHgAQSb9wYT2KZbXNIDW8\nV4rzQby6UQKBgQDjz8OAJFBzf2xiydOdCVFIcBGeroZkykRY6hVgaDbXcbEHlO5F\nJTsBMsFRQ92EclXfugMZek4jaXTQY+I5jVrQIwsbIlqXw0id08mMexcWjpp/Ry0T\nYvNI70QXJEyNGUHc8QhBeLfdL5snoRmcnKOg6amgeV1uhcTpzFWbLE/48QKBgQCU\nI/BGFqr+GQTIkkFGGqX307QaTAL7+zN1/yKf9s2lhwim/JQsFuIxmV7DCrvXRFTC\n46zl/I90PCZa8LX3VKGpXmhb1cQu0Yyy8FjCpXylNdptg12/Qz6G7SrzdBeAuA5A\n/9Wi5tQIUBzgg70GxpQMtrhC3eS8xE3rhZbr70MkAQKBgBZhkOE1tnFoJ+0MiPQ4\nTpg/BA3JJeZEYkLj4wkAmN5wAzm651Ql6hSBz/4BR7ZKdkT8WgdrDDtTubsFQ1RW\nYtsBTqTlqhYLVENMXgt7BBdGXRxU8aScE8LJcgic9vLD14BwvutFz7eOzozJD60Q\n6eFftoeqEbyw8m3uLwbMqtxR', 
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(request)
+        //   }).then(res => {
+        //     console.log("Request complete! response:", res);
+        //   });
+    } catch(err){
+        console.error(err)
+    }
+}
 
 function getMessage(){
     if (i < messages.length){
@@ -79,6 +134,7 @@ function getMessage(){
         newText.style.padding = "10px"
         newText.style.marginTop = "5px"
         newText.style.width = "80%"
+        speak(messages[i].text)
 
         if (messages[i].name === "Mike"){
             newText.style.backgroundColor = "#1E04FC"
